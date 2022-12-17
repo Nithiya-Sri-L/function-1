0b9d04f9b2b52d09efd5a6b29823b736b09007eb
nithiya <nithiyasrisri3@gmail.com>
2022-12-17 06:50:50 +0530

    adding a new file

---
+++ js/type.js
@@ -0,0 +1,29 @@
+//function with argument and with return type
+
+function nithi(a,b){
+	return a+b;
+}
+console.log(nithi(2,6))
+
+//function with argument and with whithout return type
+
+
+function nithi(a,b){
+	console.log(a+b);
+}
+nithi(6,6)
+
+//function without argument and with  return type
+
+function fun(){
+	return '3';
+}
+c=fun()
+console.log(c)
+*/
+
+//function without argument and without return type
+function fun(){
+	console.log("nithiya");
+}
+fun()
